c65aa2e61bf25514cba8af24d96bc54c603ed5b4
volynkina <bzzz18@yandex.ru>
2023-03-13 14:38:49 +0500

    Проверьте ДЗ

---
+++ src/ServiceStation.java
@@ -0,0 +1,26 @@
+public class ServiceStation {
+    public void check(Car car) {
+        checkWheeledTransport(car);
+        checkEngine(car);
+    }
+
+    public void check(Bicycle bicycle) {
+        checkWheeledTransport(bicycle);
+    }
+
+    public void check(Truck truck) {
+        checkWheeledTransport(truck);
+        checkEngine(truck);
+        truck.checkTrailer();
+        }
+
+    private void checkWheeledTransport(WheeledTransport wheeledTransport) {
+        System.out.println("Обслуживаем " + wheeledTransport.getModelName());
+        for (int i = 0; i < wheeledTransport.getWheelsCount(); i++) {
+            wheeledTransport.updateTyre();
+        }
+    }
+    private void checkEngine(Enginable enginable){
+        enginable.checkEngine();
+    }
+}
